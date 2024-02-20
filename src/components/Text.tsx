34b035d83b63c6c95411f2ead3781e8cf1e48f57
Robby Klein <hello@robbyk.com>
2024-02-20 12:59:58 -0800

    fix it?

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -3,8 +3,13 @@ import classNames from "classnames";
 interface IProps {
   className?: string;
   children?: React.ReactNode;
+  small?: boolean;
 }
 
-export default function Text({ className, children }: IProps) {
-  return <div className={classNames("text", className)}>{children}</div>;
+export default function Text({ className, children, small }: IProps) {
+  return (
+    <div className={classNames("text", { "text--small": small }, className)}>
+      {children}
+    </div>
+  );
 }
--- src/modules/FAQQuestion.tsx
+++ src/modules/FAQQuestion.tsx
@@ -43,7 +43,7 @@ export default function FAQQuestion({ className, question, children }: IProps) {
         })}
       >
         <div className="faq-question__body-inner">
-          <Text>{children}</Text>
+          <Text small>{children}</Text>
         </div>
       </div>
     </div>
