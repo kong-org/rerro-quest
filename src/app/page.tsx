091a36bac7d4cd6d5775070c4fa522c9de33055a
Cameron Robertson <charles.c.robertson@gmail.com>
2024-02-27 18:46:21 -0700

    copy fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -226,7 +226,7 @@ export default function Home() {
                 <Badge tag="h3">Premise</Badge>
 
                 <p>
-                  Chaos has erupted: the Digiphysical Man has absconded
+                  Chaos has erupted: the Digi-Physical Man has absconded
                   with invaluable artifacts (chipped merch), disrupting the
                   harmony between physical and digital realms.
                 </p>
@@ -235,7 +235,7 @@ export default function Home() {
                   Begin by collecting <strong>$RERROs</strong> scattered across
                   the event. These tokens can be found throughout ETHDenver and
                   only champions charged with enough <strong>$RERRO</strong>{" "}
-                  energy will be able to confront the Digiphysical Man and recover the
+                  energy will be able to confront the Digi-Physical Man and recover the
                   pilfered artifacts.
                 </p>
 
@@ -264,7 +264,7 @@ export default function Home() {
               <TokenBlock
                 iconSrc="/images/orb-badge.png"
                 heading="The Colossal $RERRO Orb"
-                text="Held by the Digiphysical Man"
+                text="Held by the Digi-Physical Man"
                 buttonText="3,000 TOKENS"
               />
             </div>
@@ -325,7 +325,7 @@ export default function Home() {
             <div className="pl-6 pr-6">
               <MaxWidth size="640" className="mt-12 mb-12">
                 <Text className="text-center">
-                  Chaos has erupted: the Digiphysical Man has absconded
+                  Chaos has erupted: the Digi-Physical Man has absconded
                   with invaluable artefacts (chipped merch), disrupting the
                   harmony between physical and digital realms.
                 </Text>
